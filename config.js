11e8ab581046f9b7b3596b7b9728a0d8ff4ed15e
yahambotmddeploy <hiruninishanshala8@gmail.com>
2024-12-13 13:23:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session is",  //session id dhan
+SESSION_ID: process.env.SESSION_ID || "HIw2CQiQ#AGsxOf-iARQHXQqEdAICG4E9PaehI1Ko_HY5Cxe1L_w",  //session id dhan
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/2qD0Syx/6648.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
-SUDO_NB: process.env.SUDO_NB || "94774609569",
+SUDO_NB: process.env.SUDO_NB || "94756209818",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE:"true"
+AUTO_VOICE:"false"
 };
 
